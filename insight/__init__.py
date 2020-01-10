9d54f1cfcb5eae923d82a6e9e89550a40562e679
Richard Scheiwe <richard.s@taboola.com>
2020-01-10 20:59:31 +0200

    rebased boot structure

--- insight/__init__.py
+++ insight/__init__.py
@@ -1,5 +1,16 @@
+import os
 from flask import Flask
 
-app = Flask(__name__)
+from .config import config_by_name
 
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config_by_name[config_name])
+    return app
+
+
+# app = Flask(__name__)
+
+app = create_app(os.getenv('BOILERTPLATE_ENV') or 'dev')
 from insight.controller import routes
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-app="ihp-api"
+app="insight-api"
 docker build -t ${app} .
-docker run -d -p 2222:80 \
+docker run -d -p 2223:80 \
   --name=${app} \
   -v $PWD:/app ${app}
