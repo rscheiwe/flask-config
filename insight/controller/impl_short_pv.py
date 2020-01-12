ac9e03f28b88c508e5a53b4bb9e277f15b1bfbbc
Richard Scheiwe <richard.s@taboola.com>
2020-01-12 10:33:30 +0200

    refactored param parsing

--- insight/controller/impl_short_pv.py
+++ insight/controller/impl_short_pv.py
@@ -1,6 +1,6 @@
 import os
 import json
-from flask import jsonify
+from flask import request
 from flask_restplus import Resource, reqparse, marshal, fields
 
 from insight.util.json_read_write import load_json
@@ -22,12 +22,16 @@ my_resource_parser = reqparse.RequestParser()
 my_resource_parser.add_argument('publisher', type=str, default='string: name', required=True)
 
 @pv_api.route('')
-# @pv_api.param('publisher_id', 'The numerical publisher ID')
+@pv_api.param('publisher_id', 'The numerical publisher ID')
 class ImplShortPv(Resource):
     @pv_api.marshal_with(pv_model)
     @pv_api.response(400, 'Bad Request -- Publisher required')
     # @pv_api.expect(my_resource_parser)
     def get(self):
+        if not request or request.args.get('publisher') is None:
+            abort(400, description='Publisher required')
+
+        param = request.args.get('publisher', type=int)
         # parser = reqparse.RequestParser()
         # parser.add_argument('publisher', type=int, location='args')
         # args = my_resource_parser.parse_args()
